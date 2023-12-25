aa987a98380f5d2b27f71810bc5ebbd8ca3d3d6f
Chris <stefaniakchristopher@gmail.com>
2023-12-25 02:04:14 -0500

    sign up static elements

--- pp-frontend/src/routes/signup/+page.svelte
+++ pp-frontend/src/routes/signup/+page.svelte
@@ -1,12 +1,79 @@
+<div class="body">
+    <form class="signup-container">
+        <h1>Sign Up</h1>
 
-<h1>Sign Up</h1>
-<form>
-    <label for="usernameInput">Username</label>
-    <input id="usernameInput" name="usernameInput" type="text" />
+        <label for="emailInput">Email</label>
+        <input id="emailInput" name="emailInput" type="text" />
 
-    <label for="passwordInput">Password</label>
-    <input id="passwordInput" name="passwordInput" type="password" />
+        <label for="usernameInput">Username</label>
+        <input id="usernameInput" name="usernameInput" type="text" />
 
-</form>
+        <label for="passwordInput">Password</label>
+        <input id="passwordInput" name="passwordInput" type="password" />
 
-<style></style>
+        <label for="repeatPasswordInput">Repeat Password</label>
+        <input id="repeatPasswordInput" name="repeatPasswordInput" type="password" />
+
+        <button disabled>Submit</button>
+    </form>
+</div>
+
+<style>
+    :global(body) {
+        background-color: #0D1321;
+        font-family: Arial, sans-serif;
+        background-color: #0D1321;
+        justify-content: center;
+        align-items: center; 
+        display: flex;
+        height: 100vh;
+        margin: 0;
+        padding: 0;
+    }
+
+    .signup-container {
+        background-color: #FFEDDF;
+        padding: 30px;
+        border-radius: 8px;
+        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+        width: 300px;
+        align-self: auto;
+    }
+
+    h1 {
+        text-align: center;
+        color: #0D1321;
+    }
+
+    label {
+        display: block;
+        margin-bottom: 6px;
+        color: #0D1321; /* Change label color to green */
+    }
+
+    input[type="text"],
+    input[type="password"] {
+        width: 100%;
+        padding: 8px;
+        margin-bottom: 15px;
+        border: 1px solid #ccc;
+        border-radius: 4px;
+        box-sizing: border-box;
+        
+    }
+
+    button {
+        width: 100%;
+        padding: 10px;
+        background-color: #4caf50;
+        color: #fff;
+        border: none;
+        border-radius: 4px;
+        cursor: pointer;
+        transition: background-color 0.3s ease;
+    }
+
+    button:hover {
+        background-color: #45a049;
+    }
+</style>
--- pp-frontend/src/routes/signup/SignUpPage.spec.js
+++ pp-frontend/src/routes/signup/SignUpPage.spec.js
@@ -1,8 +1,9 @@
 // @ts-nocheck
-import { render, screen } from '@testing-library/svelte';
+import { render, screen} from '@testing-library/svelte';
 // @ts-ignore
 import SignUpPage from './+page.svelte';
 import "@testing-library/jest-dom";
+import userEvent from "@testing-library/user-event";
 
 
 describe('Sign Up Page', () => {
@@ -25,11 +26,38 @@ describe('Sign Up Page', () => {
         expect(usernameInput).toBeInTheDocument();
     })
 
+    it('has a repeat password input', () => {
+        render(SignUpPage);
+        const repeatPasswordInput = screen.getByLabelText("Repeat Password", { label: "repeatPasswordInput"}, { type : "password"});
+        expect(repeatPasswordInput).toBeInTheDocument();
+    })
+
+    it('has a email input', () => {
+        render(SignUpPage);
+        const emailInput = screen.getByLabelText("Email", { label: "emailInput"});
+        expect(emailInput).toBeInTheDocument();
+    })
+
     it('has a submit button', () => {
         render(SignUpPage);
-        const passwordInput = screen.get
-        expect(usernameInput).toBeInTheDocument();
+        const submitButton = screen.getByRole("button", { name: "Submit" });
+        expect(submitButton).toBeInTheDocument();
     })
     
+    it('that submit button starts off disabled', ()=> {
+        render(SignUpPage)
+        const submitButton = screen.getByRole("button", { name: "Submit" });
+        expect(submitButton).toBeDisabled()
+    })
+
+    it('but once the inputs are filled out and the passwords match it becomes enabled', async ()=> {
+        render(SignUpPage)
+        const submitButton = screen.getByRole("button", { name: "Submit" });
+        await userEvent.type(emailInput, "something@mail.com")
+        await userEvent.type(usernameInput, "muhammad")
+        await userEvent.type(passwordInput, "muhammad123")
+        await userEvent.type(repeatPasswordInput, "muhammad123")
+        expect(submitButton).toBeEnabled()
+    })
 })
 
