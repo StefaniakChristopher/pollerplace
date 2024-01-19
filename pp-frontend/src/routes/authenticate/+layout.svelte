8e8a611b645c22b4cfc2f3169e1c9c2da725bf17
Chris <stefaniakchristopher@gmail.com>
2024-01-19 17:20:12 -0500

    background color under control

--- pp-frontend/src/routes/authenticate/+layout.svelte
+++ pp-frontend/src/routes/authenticate/+layout.svelte
@@ -1,21 +1,23 @@
-<body>
+
+<div class="signuporlogin-container-alignment">
     <form class="signuporlogin-container">
         <slot/>
     </form>
-</body>
-
-
+</div>
 
 
 <style>
-    body {
+
+    :global(body) {
         background-color: #0D1321;
+        height: 98vh
+    }
+    .signuporlogin-container-alignment {
         justify-content: center;
         align-items: center;
         display: flex;
-        height: 100vh
+        height: 98vh
     }
-
     .signuporlogin-container {
         
         background-color: #FFEDDF;
@@ -25,6 +27,4 @@
         width: 300px;
         align-self: auto;
     }
-    
-   
 </style>
