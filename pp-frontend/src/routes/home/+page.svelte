13a7f967ae7abf04d94eb2e9521cc2f1c7abea57
Chris <stefaniakchristopher@gmail.com>
2024-01-22 13:56:11 -0500

    create poll button implemented

--- pp-frontend/src/routes/home/+page.svelte
+++ pp-frontend/src/routes/home/+page.svelte
@@ -1,72 +1,107 @@
-<script>
-    
+<script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js">
+  import { onMount } from "svelte";
+
+  // if no onMount, then 'window not defined error'
+  onMount(() => {
+    // @ts-ignore
+    import('boxicons');
+  });
 </script>
 
 <!-- make a create poll option with a different style of button -->
 <nav class="topBar">
-    <img src='src/static/pplogo.png' alt="Poller Place Logo" class="upper-left-logo">
+  <img
+    src="src/static/pplogo.png"
+    alt="Poller Place Logo"
+    class="upper-left-logo"
+  />
 
-    <input type="text" class="search-input" placeholder="Search...">
+  <input type="text" class="search-input" placeholder="Search..." />
 
-    <button class="signup-and-login-button" id="login-button">Login</button>
-    <button class="signup-and-login-button" id="signup-button">Sign Up</button>
+    
+  <div class="top-bar-buttons" id="create-poll-div">
+    <box-icon id='create-poll-plus' name='plus'></box-icon>
+    <button class="top-bar-buttons" id="create-poll-button">Create Poll</button>
+  </div>
+
+
+  <button class="top-bar-buttons" id="login-button">Login</button>
+  <button class="top-bar-buttons" id="signup-button">Sign Up</button>
 </nav>
 
+
+
 <style>
-    :global(body) {
-        background-color: #0D1321;
-        height: 98vh
-    }
+  :global(body) {
+    background-color: #0d1321;
+    height: 98vh;
+  }
 
-    .upper-left-logo {
-        width: 5%;
-        padding: 1%;
-    }
-    .topBar {
-        display: flex;
-    }
+  .upper-left-logo {
+    width: 5%;
+    padding: 1%;
+  }
+  .topBar {
+    display: flex;
+    background-color: #9fa58e;
+  }
 
-    .search-input {
-        padding: 10px;
-        border: none;
-        border-radius: 25px;
-        font-size: 16px;
-        outline: none;
-        margin-left: 3%;
-        width: 75%;
-        margin-bottom: 2%;
-        margin-top: 2%;
-        display: flex;
-        background-color: #ffeee0;
-        margin-right: 5%;
-    }
+  .search-input {
+    padding: 10px;
+    border: none;
+    border-radius: 25px;
+    font-size: 16px;
+    outline: none;
+    margin-left: 3%;
+    width: 75%;
+    margin-bottom: 2%;
+    margin-top: 2%;
+    display: flex;
+    background-color: #ffeee0;
+    margin-right: 5%;
+  }
 
-    .signup-and-login-button {
-        margin-top: 2%;
-        margin-bottom: 2%;
-        margin-right: 1%;
-        width:5%;
-        background-color: #ffeee0;
-        border: none;
-        border-radius: 25px;
-    }
+  #create-poll-div{
+    border-radius: 0px;
+    width: 7%;
+    display: flex;
+    align-items: center;
+  }
 
-    .signup-and-login-button,
-    .search-input {
-        transition: background-color 1s ease; 
-    }
+  .top-bar-buttons {
+    margin-top: 2%;
+    margin-bottom: 2%;
+    margin-right: 1%;
+    width: 6%;
+    background-color: #ffeee0;
+    border: none;
+    border-radius: 25px;
+  }
 
+  .top-bar-buttons,
+  .search-input {
+    transition: background-color 1s ease;
+  }
 
-    .signup-and-login-button:hover {
-        background-color: aliceblue;
-        cursor: pointer;
-    }
+  .top-bar-buttons:hover {
+    background-color: aliceblue;
+    cursor: pointer;
+  }
 
-    .search-input:focus {
-        background-color: aliceblue
-    }
-/* enforce a minimum sizing on some of these so it doesnt look stoopid */
+  .search-input:focus {
+    background-color: aliceblue;
+  }
+
+  #create-poll-button {
+    width: 80%;
     
+  }
 
-   
+  #create-poll-plus {
+    margin-left: 8%;
+  }
+  
+
+
+  /* enforce a minimum sizing on some of these so it doesnt look stoopid */
 </style>
